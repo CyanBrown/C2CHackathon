afa87ddcd279191eff313d2eb41e798cb39ae8ab
CyanBrown <cyanbrown29@gmail.com>
2021-03-19 17:57:06 -0500

    formula examples

---
+++ formulas.py
@@ -0,0 +1,14 @@
+import math
+
+def circumference_circle(radius):
+    return 2*math.pi*radius
+
+def area_circle(radius):
+    return math.pi*(radius**2)
+
+def surface_area_sphere(radius):
+    return 4*math.pi*(radius**2)
+
+def volume_sphere(radius):
+    return (4/3)*math.pi*(radius**3)
+
