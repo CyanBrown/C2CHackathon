1c642ec67ecb064fee7ce877a63407d3e158562b
CyanBrown <cyanbrown29@gmail.com>
2021-03-20 11:27:44 -0500

    finished

--- formulas.py
+++ formulas.py
@@ -8,40 +8,59 @@ class CircleSphere:
         self.surface_area = 4 * math.pi * (radius ** 2)
         self.volume = (4 / 3) * math.pi * (radius ** 3)
 
+    def __str__(self):
+        return f"\nCircumference: {self.circumference}\nArea: {self.area}\nSurface Area: {self.surface_area}\nVolume: {self.volume}\n"
+
 
 class SquareRect:
     def __init__(self, side1, side2):
         self.perimeter = 2 * (side1 + side2)
         self.area = side1 * side2
 
+    def __str__(self):
+        return f"\nPerimeter: {self.perimeter}\nArea: {self.area}"
+
 
 class CubeRect:
-    def __init__(self, l, w, h):
+    def __init__(self, length, width, height):
+        l=length
+        w = width
+        h = height
         self.volume = l * w * h
         self.surface_area = ((l * h) * 4) + (h * w) * 2
 
+    def __str__(self):
+        return f"\nSurface Area: {self.surface_area}\nVolume: {self.volume}\n"
 
 class Trapezoid:
     def __init__(self, base1, base2, height):
         self.area = ((base1 + base2) / 2) * height
 
+    def __str__(self):
+        return f"\nArea: {self.area}\n"
 
 class Cone:
     def __init__(self, radius, height):
         self.surface_area = math.pi * radius * height
         self.volume = (1 / 3) * math.pi * (radius ** 2) * height
 
-
+    def __str__(self):
+        return f"\nSurface Area: {self.surface_area}\nVolume: {self.volume}\n"
 class Cylinder:
     def __init__(self, radius, height):
         self.surface_area = 2 * math.pi * radius * height + (2 * math.pi * (radius ** 2))
         self.volume = math.pi * (radius ** 2) * height
 
+    def __str__(self):
+        return f"\nSurface Area: {self.surface_area}\nVolume: {self.volume}\n"
 
 class NthPolygon:
     def __init__(self, side_length, side_count):
         self.perimeter = side_length * side_count
         self.area = self.perimeter * .5 * (side_length / (2 * math.tan(math.radians(180 / side_count))))
+
+    def __str__(self):
+        return f"\nPerimeter: {self.perimeter}\nArea: {self.area}\n"
 
 
 class Triangle:
--- main.py
+++ main.py
@@ -2,59 +2,69 @@ import sys
 from rich import *
 from rich.table import Table
 from rich.console import Console
+from formulas import *
+import inspect
 
 
-
-def gui():
-    index = 0
-
-    selection_table = Table(title="Select which calculator you would like to use")
-
-    selection_table.add_column("Index", justify="right", style="Red", no_wrap=True)
-    selection_table.add_column("Calculator", justify="right", style="cyan", no_wrap=True)
-
-    selection_table.add_row("1", "Sphere")
-    selection_table.add_row("2", "Square/Rectangle")
-    selection_table.add_row("3", "Cube")
-    selection_table.add_row("4", "Trapezoid")
-    selection_table.add_row("5", "Cone")
-    selection_table.add_row("6", "Cylinder")
-    selection_table.add_row("7", "Polygon (n sides)")
-    selection_table.add_row("8", "Triangle")
-    selection_table.add_row("9", "Sine")
-    selection_table.add_row("10", "Cosine")
-    selection_table.add_row("11", "Tangent")
-    selection_table.add_row("12", "Arcsin")
-    selection_table.add_row("13", "Arccos")
-    selection_table.add_row("14", "Arctan")
-
-    console = Console()
-    console.print(selection_table)
-
-    index = input("Type the index of which calculator you want: ")
 
 
 def main():
-    
-    gui()
 
-    while not exit:
-        # in here put the main code that will repeat as long as the user wants
-        ...
-        """
-        return and closing logic statements
-        
-        if user_input = "return":
-            continue
-        
-        elif user_input = "exit":
-            exit = True
-        
-        """
+    classes = [CircleSphere, SquareRect, CubeRect, Trapezoid, Cone, Cylinder, NthPolygon]
 
-    """
-    maybe add final goodbye
-    """
+    ex = False
+
+    while not ex:
+        selection_table = Table(title="Select which calculator you would like to use")
+
+        selection_table.add_column("Index", justify="right", style="Red", no_wrap=True)
+        selection_table.add_column("Calculator", justify="right", style="cyan", no_wrap=True)
+
+        selection_table.add_row("1", "Circle/Sphere")
+        selection_table.add_row("2", "Square/Rectangle")
+        selection_table.add_row("3", "Cube/Rectangular Prism")
+        selection_table.add_row("4", "Trapezoid")
+        selection_table.add_row("5", "Cone")
+        selection_table.add_row("6", "Cylinder")
+        selection_table.add_row("7", "Polygon (n sides)")
+        # selection_table.add_row("8", "Triangle")
+        # selection_table.add_row("9", "Sine")
+        # selection_table.add_row("10", "Cosine")
+        # selection_table.add_row("11", "Tangent")
+        # selection_table.add_row("12", "Arcsin")
+        # selection_table.add_row("13", "Arccos")
+        # selection_table.add_row("14", "Arctan")
+
+        console = Console()
+        console.print(selection_table)
+
+        while True:
+            try:
+                index = int(input("Type the number of which calculator you want: "))-1
+                choice = classes[index]
+                break
+            except:
+                print("That is not a valid input, please try again")
+
+        params = inspect.signature(choice.__init__)
+        params = list(str(params).replace(")","").split(", "))[1:]
+
+        ps = []
+
+        for i in params:
+            try:
+                ps.append(float(input(f"What are the {i}: ")))
+            except:
+                print("Invalid Input")
+                continue
+
+        obj = choice(*ps)
+
+        print(obj)
+
+        t = input("Type 'return' to go back to the home page and anything else to quit: ")
+        if t!='return':
+            quit(0)
 
 if __name__ == '__main__':
     main()
