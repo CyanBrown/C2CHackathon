5bb6a7584964de672676de73ef5244437e989f33
CyanBrown <cyanbrown29@gmail.com>
2021-03-19 21:12:44 -0500

    put formulas into classes

--- formulas.py
+++ formulas.py
@@ -1,49 +1,41 @@
 import math
 
-def circumference_circle(radius):
-    return 2*math.pi*radius
+class CircleSphere():
+    def __init__(self, radius):
+        self.circumference = 2 * math.pi * radius
+        self.area = math.pi * (radius ** 2)
+        self.surface_area = 4 * math.pi * (radius ** 2)
+        self.volume = (4 / 3) * math.pi * (radius ** 3)
 
-def area_circle(radius):
-    return math.pi*(radius**2)
+class SquareRect():
+    def __init__(self, side1, side2):
+        self.perimeter = 2 * (side1 + side2)
+        self.area = side1*side2
 
-def surface_area_sphere(radius):
-    return 4*math.pi*(radius**2)
+class CubeRect():
+    def __init__(self, l, w, h):
+        self.volume = l*w*h
+        self.surface_area = ((l*h)*4)+(h*w)*2
 
-def volume_sphere(radius):
-    return (4/3)*math.pi*(radius**3)
+class Trapezoid():
+    def __init__(self,base1, base2, height):
+        self.area = ((base1+base2)/2)*height
 
-def perimeter_square_or_rect(side1,side2):
-    return 2*(side1+side2)
+class Cone():
+    def __init__(self, radius, height):
+        self.surface_area = math.pi * radius * height
+        self.volume = (1 / 3) * math.pi * (radius ** 2) * height
 
-def area_square_or_rect(side1,side2):
-    return side1*side2
+class Cylinder():
+    def __init__(self, radius, height):
+        self.surface_area = 2 * math.pi * radius * height + (2 * math.pi * (radius ** 2))
+        self.volume = math.pi * (radius ** 2) * height
 
-#TODO: add surface area and volume cube and rectangular prism
+class NthPolygon():
+    def __init__(self, side_length, side_count):
+        self.perimeter = side_length*side_count
+        self.area = self.perimeter*.5*(side_length/(2*math.tan(math.radians(180/side_count))))
 
-def area_triangle(base, height):
-    return .5*base*height
-
-def area_trapezoid(base1,base2,height):
-    return .5*(base1+base2)*height
-
-def surface_area_cone(radius,height):
-    return math.pi*radius*height
-
-def volume_cone(radius, height):
-    return (1/3)*math.pi*(radius**2)*height
-
-def surface_area_cylinder(radius, height):
-    return 2*math.pi*radius*height+(2*math.pi*(radius**2))
-
-def volume_cylinder(radius, height):
-    return math.pi*(radius**2)*height
-
-def perimeter_nth_polygon(side_length, side_count):
-    return side_length*side_count
-
-def area_nth_polygon(apothem, side, side_count):
-    return perimeter_nth_polygon(side, side_count)*.5*apothem
-
-#TODO: maybe add non-apothem way of calculating area
-
-
+class Triangle()
+    def __init__(self):
+        ...
