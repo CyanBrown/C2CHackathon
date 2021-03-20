47a2bcd8146602b3935fb6974ca9f69a60ed5005
CyanBrown <cyanbrown29@gmail.com>
2021-03-19 21:13:29 -0500

    put formulas into classes

--- formulas.py
+++ formulas.py
@@ -1,41 +1,41 @@
 import math
 
-class CircleSphere():
+class CircleSphere:
     def __init__(self, radius):
         self.circumference = 2 * math.pi * radius
         self.area = math.pi * (radius ** 2)
         self.surface_area = 4 * math.pi * (radius ** 2)
         self.volume = (4 / 3) * math.pi * (radius ** 3)
 
-class SquareRect():
+class SquareRect:
     def __init__(self, side1, side2):
         self.perimeter = 2 * (side1 + side2)
         self.area = side1*side2
 
-class CubeRect():
+class CubeRect:
     def __init__(self, l, w, h):
         self.volume = l*w*h
         self.surface_area = ((l*h)*4)+(h*w)*2
 
-class Trapezoid():
+class Trapezoid:
     def __init__(self,base1, base2, height):
         self.area = ((base1+base2)/2)*height
 
-class Cone():
+class Cone:
     def __init__(self, radius, height):
         self.surface_area = math.pi * radius * height
         self.volume = (1 / 3) * math.pi * (radius ** 2) * height
 
-class Cylinder():
+class Cylinder:
     def __init__(self, radius, height):
         self.surface_area = 2 * math.pi * radius * height + (2 * math.pi * (radius ** 2))
         self.volume = math.pi * (radius ** 2) * height
 
-class NthPolygon():
+class NthPolygon:
     def __init__(self, side_length, side_count):
         self.perimeter = side_length*side_count
         self.area = self.perimeter*.5*(side_length/(2*math.tan(math.radians(180/side_count))))
 
-class Triangle()
+class Triangle:
     def __init__(self):
         ...
